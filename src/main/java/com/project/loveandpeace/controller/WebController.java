cfd36b12330ea088b7f7b252b0cdcffd4a028212
jiyounSeo <joanne901106@gmail.com>
2019-07-12 18:56:24 +0900

    template layout 나누기 sample code

--- src/main/java/com/project/loveandpeace/controller/WebController.java
+++ src/main/java/com/project/loveandpeace/controller/WebController.java
@@ -8,6 +8,6 @@ public class WebController {
 
     @GetMapping("/")
     public String main() {
-        return "main";
+        return "index";
     }
 }
--- src/main/resources/test.sql
+++ src/main/resources/test.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE `loveandpeace` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE `loveandcash` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 CREATE TABLE `notice` (
   `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -7,4 +7,4 @@ CREATE TABLE `notice` (
   `create_dt` datetime(6) DEFAULT NULL,
   `modify_dt` datetime(6) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
